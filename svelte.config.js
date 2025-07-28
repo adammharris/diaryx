0cdf107304e0e334d427403765c30f1f38b818e9
adammharris <97053407+adammharris@users.noreply.github.com>
2025-07-28 10:01:12 -0600

    fix: Remove unused build command and adjust SvelteKit adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -5,11 +5,11 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-          fallback: 'index.html',
-          pages: 'build',
-          assets: 'build',
-          strict: false
-        }),
+    adapter: adapterStatic({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    })
   },
 };
 
